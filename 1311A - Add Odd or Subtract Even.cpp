505db757ae094516c2411d8f6224baef63cd049f
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-01-03 00:56:31 +0600

    Add files via upload

---
+++ 1311A - Add Odd or Subtract Even.cpp
@@ -0,0 +1,45 @@
+//unsubmit
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,a,b,i,j,x,y,flag,sum,k,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d %d",&a,&b);
+        if(a==b)
+        {
+            flag = 0;
+        }
+        else if(b>a&&(b-a)%2==1)
+        {
+            flag = 1;
+        }
+        else if(b>a&&(b-a)%2==0)
+        {
+            flag = 2;
+        }
+        else if(b<a&&(a-b)%2==1)
+        {
+            flag = 2;
+        }
+        else{
+            flag = 1;
+        }
+
+        printf("%d\n",flag);
+
+
+
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
